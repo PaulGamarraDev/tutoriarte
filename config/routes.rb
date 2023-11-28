ff6655e3ff768ec8a032ef061548b9af99309f3c
wind peña <nirvanavid@gmail.com>
2023-11-28 13:35:07 -0300

    Creando el contolador Bookimg con las 7 acciones

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'booking/new'
+  get 'booking/create'
+  get 'booking/edit'
+  get 'booking/update'
+  get 'booking/destroy'
+  get 'booking/show'
+  get 'booking/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

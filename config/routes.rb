0ce2e1629d131f2b7baab666b08b993cd0fc3f2b
wind peña <nirvanavid@gmail.com>
2023-11-30 21:23:00 -0300

    correccion de las rutas en routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  get 'booking/new'
-  get 'booking/create'
-  get 'booking/edit'
-  get 'booking/update'
-  get 'booking/destroy'
-  get 'booking/show'
-  get 'booking/index'
+  
   devise_for :users
+  resources :bookings
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 end
+
+

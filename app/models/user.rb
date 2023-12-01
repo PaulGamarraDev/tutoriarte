b757c5daec1b1a8cde356b571f20e00f65a435ec
Paul Gamarra <paulgamarra.dev@gmail.com>
2023-11-30 21:27:39 -0300

    added User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
+  validates :first_name, :last_name, :role, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  ROLES = [[0, 'Teacher'], [1, 'Student']]
+
+  enum role: %i[teacher student]
+
+  after_initialize :set_default_role, if: :new_record?
+
+  def set_default_role
+    self.role ||= :student
+  end
 end
